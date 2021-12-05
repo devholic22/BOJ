5c9b41b4bdf65e91ca89a2fd5667bac6dc81fc65
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-12-05 21:44:14 +0900

    Create 1110.py

---
+++ 단계별로 풀어보기/while문/1110.py
@@ -0,0 +1,16 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+
+if (0 <= n <= 99):
+    count = 0
+    value = n
+    arr = []
+    arr.append(value)
+    while arr.count(value) < 2:
+        arr.append(value)
+        first = value // 10
+        second = value % 10
+        sum = first + second
+        count += 1
+        value = (second * 10) + (sum % 10)
+    print(count)
