c1bf958cc28302d5a09a2d8a9cd079485bc5562a
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-12-05 20:54:47 +0900

    Create 11022.py

---
+++ 단계별로 풀어보기/for문/11022.py
@@ -0,0 +1,8 @@
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+
+for i in range(1, T + 1):
+    A, B = map(int, sys.stdin.readline().rstrip().split())
+    if (0 < A < 10 and 0 < B < 10):
+        print(f"Case #{i}: {A} + {B} = {A + B}")
