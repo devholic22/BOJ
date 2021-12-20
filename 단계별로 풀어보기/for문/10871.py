c908fd3cdbf1a5c3a65cc67f5192d72ad042ee12
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2021-12-20 20:32:33 +0900

    Update 10871.py

--- 단계별로 풀어보기/for문/10871.py
+++ 단계별로 풀어보기/for문/10871.py
@@ -2,13 +2,8 @@ import sys
 
 N, X = map(int, sys.stdin.readline().rstrip().split())
 
-arr = []
-
-value = sys.stdin.readline().rstrip().split()
+value = list(map(int, sys.stdin.readline().rstrip().split()))
 
 for i in range(N):
-    if int(value[i]) < X:
-        arr.append(value[i])
-
-for result in arr:
-    print(result, end=" ")
+    if value[i] < X:
+        print(value[i], end=" ")
